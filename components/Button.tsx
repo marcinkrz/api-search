40c5031f6c81ab1a20f88f8ecab9923e5aa219d2
Marcin Krzaczyński <mkrzaczynski@gmail.com>
2026-05-25 16:56:02 +0200

    Fix select height and revert button colors

--- components/Button.tsx
+++ components/Button.tsx
@@ -20,9 +20,9 @@ const buttonVariants = cva(
       variant: {
         primary:
           `bg-[var(--foreground)] text-[var(--background)] 
-          hover:bg-[var(--primary)] 
+          hover:bg-[var(--foreground-1)] 
           focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[var(--foreground)]
-          active:bg-[var(--primary)]
+          active:bg-[var(--foreground-1)]
           disabled:bg-[var(--background-3)] disabled:border-[var(--background-3)] disabled:hover:border-[var(--background-3)]`,
         outline:
           `bg-transparent border-[var(--border)] text-[var(--foreground-1)] 
